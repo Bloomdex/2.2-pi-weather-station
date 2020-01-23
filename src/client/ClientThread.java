5caa829b31c775eba1eea8498d5b9a3951a62d11
Bart <bartkuiper13@gmail.com>
2020-01-23 23:20:54 +0100

    Made a SSL encrypted client and a test program.

---
+++ src/client/ClientThread.java
@@ -0,0 +1,55 @@
+package client;
+
+import javax.net.ssl.SSLSocket;
+import java.io.*;
+import java.net.Socket;
+
+public class ClientThread extends Thread{
+    private final Socket socket;
+    private static byte[] data;
+    private DataOutputStream dataOutputStream;
+
+    /**
+     * The constructor witch sets the socket and the dataOutputStream
+     *
+     * @param socket the socket witch allows the client to send data
+     * @throws IOException throws an exception if something went wrong with getting the output stream from the socket
+     */
+    ClientThread(SSLSocket socket) throws IOException {
+        this.socket = socket;
+        dataOutputStream = new DataOutputStream(this.socket.getOutputStream());
+    }
+
+    /**
+     * The function that sends the data
+     */
+    public void run() {
+        try {
+            while(!socket.isClosed()) {
+                synchronized (this) {
+                    this.wait();
+                    dataOutputStream.writeInt(data.length); // write length of the message
+                    dataOutputStream.write(data);
+                    /*
+                    for (int x = 0; x <= data.length - 1; x++) {
+                        System.out.print((char)data[x]);
+                    }
+                    System.out.print("\n");
+                     */
+                }
+            }
+        } catch (IOException | InterruptedException e) {
+            Thread.currentThread().interrupt();
+            System.out.printf("exception: %s%n", e.getMessage());
+        }
+    }
+
+    /**
+     * Sets the data witch will be sent
+     *
+     * @param data witch will be sent
+     */
+    static public void setData(byte[] data) {
+        ClientThread.data = data;
+    }
+}
---
+++ src/client/Test.java
@@ -0,0 +1,47 @@
+package client;
+
+import java.util.Scanner;
+
+public class Test {
+    private Client client;
+
+    /**
+     * The constructor of the Test class
+     *
+     * @throws Exception when something goes wrong with making the client
+     */
+    Test() throws Exception {
+        // Sets the global variable client
+        client = new Client();
+        // Calls the method sendData witch will handel generate data for the client thread
+        sendData();
+    }
+
+    /**
+     * Sends the incoming data from the user to the client
+     */
+    public void sendData() {
+        // A new user input scanner will be made
+        Scanner in = new Scanner(System.in);
+        while (true) {
+            // Reads the user input when the user has pressed enter
+            String s = in. nextLine();
+            // Makes from the user input a byte[]
+            byte[] byteArray = s.getBytes();
+            // Gives the data to the client who will send this data to te server
+            // The client will be notified that there is new data witch can be send
+            client.setData(byteArray);
+        }
+    }
+
+    /**
+     * The main function of the test class
+     *
+     * @param args there are no arguments that can be given
+     * @throws Exception when somethings goes wrong with making a new test class
+     */
+    public static void main(String[] args) throws Exception {
+        // Builds a new test class
+        new Test();
+    }
+}
